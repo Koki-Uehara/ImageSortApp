c8716083134a4d8b90e62a0928a84ededa6b6694
koki uehara <koki.uehara@hutzper.com>
2023-11-28 21:32:56 +0900

    画像データの整理用にスキップと移動機能を持たせたタイプ

--- MainForm.cs
+++ MainForm.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using System.Threading.Tasks;
+using System.Drawing;
 
 namespace ImageSortApp
 {
@@ -13,72 +13,106 @@ namespace ImageSortApp
             InitializeComponent();
         }
 
-        private async void MainForm_DragDrop(object sender, DragEventArgs e)
-        {
-            try
-            {
-                if (e.Data != null)
-                {
-                    var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-                    foreach (var file in files)
-                    {
-                        Console.WriteLine("Processing " + file);
-                        await ProcessDirectory(file);
-                        Console.WriteLine("Finished processing " + file);
-                    }
-
-                    MessageBox.Show("完了しました");
-
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
         private void MainForm_DragEnter(object sender, DragEventArgs e)
         {
-            if (e.Data != null)
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            var imageFiles = Directory.GetFiles(files[0], "*.*", SearchOption.TopDirectoryOnly)
+                .Where(s => s.EndsWith(".png") || s.EndsWith(".jpg") || s.EndsWith(".jpeg") || s.EndsWith(".bmp") || s.EndsWith(".gif")).ToArray();
+            if (imageFiles.Length > 0)
             {
-                if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                    e.Effect = DragDropEffects.Copy;
+                var imageForm = new ImageForm(imageFiles, files[0]);
+                imageForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("画像ファイルがありません");
+            }
+        }
+    }
+
+    public class ImageForm : Form
+    {
+        private string[] imageFiles;
+        private string sourceFolder;
+        private int currentImageIndex = 0;
+        private PictureBox pictureBox;
+        private Button btnDelete;
+        private Button btnSkip;
+        private int skipFileIndex = 1;
+
+        public ImageForm(string[] imageFiles, string sourceFolder)
+        {
+            this.imageFiles = imageFiles;
+            this.sourceFolder = sourceFolder;
+            this.pictureBox = new PictureBox { Dock = DockStyle.Fill, SizeMode = PictureBoxSizeMode.Zoom };
+            this.Controls.Add(pictureBox);
+            this.btnDelete = new Button { Text = "Delete", Dock = DockStyle.Bottom, Height = 50 };
+            this.btnDelete.Click += btnDelete_Click;
+            this.Controls.Add(btnDelete);
+            this.btnSkip = new Button { Text = "Skip", Dock = DockStyle.Bottom, Height = 50 };
+            this.btnSkip.Click += btnSkip_Click;
+            this.Controls.Add(btnSkip);
+            var destinationFolder = Path.Combine(sourceFolder, "SkippedImages");
+            if (Directory.Exists(destinationFolder))
+            {
+                var existingFiles = Directory.GetFiles(destinationFolder, "*.*", SearchOption.TopDirectoryOnly)
+                    .Where(s => s.StartsWith(Path.GetFileName(sourceFolder)));
+                if (existingFiles.Any())
+                {
+                    var lastFileName = existingFiles.OrderBy(f => f).Last();
+                    var lastFileNumber = int.Parse(Path.GetFileNameWithoutExtension(lastFileName).Substring(Path.GetFileName(sourceFolder).Length, 4));
+                    skipFileIndex = lastFileNumber + 1;
+                }
+            }
+            ShowCurrentImage();
+        }
+
+        private void ShowCurrentImage()
+        {
+            if (currentImageIndex < imageFiles.Length)
+            {
+                using (var img = new Bitmap(imageFiles[currentImageIndex]))
+                {
+                    pictureBox.Image = new Bitmap(img);
+                }
+            }
+            else
+            {
+                MessageBox.Show("画像ファイルがありません");
+                this.Close();
             }
         }
 
-        private async Task ProcessDirectory(string sourceFolder)
+        private void btnDelete_Click(object sender, EventArgs e)
         {
-            var imageFiles = Directory.GetFiles(sourceFolder, "*.*", SearchOption.TopDirectoryOnly)
-                .Where(s => s.EndsWith(".png") || s.EndsWith(".jpg") || s.EndsWith(".jpeg") || s.EndsWith(".bmp") || s.EndsWith(".gif"));
+            File.Delete(imageFiles[currentImageIndex]);
+            currentImageIndex++;
+            ShowCurrentImage();
+        }
 
-            int folderIndex = 1;
-            int fileIndex = 0;
-            int imageIndex = 1;
-
-            foreach (var imageFile in imageFiles)
+        private void btnSkip_Click(object sender, EventArgs e)
+        {
+            var destinationFolder = Path.Combine(sourceFolder, "SkippedImages");
+            Directory.CreateDirectory(destinationFolder);
+            var fileName = Path.GetFileName(imageFiles[currentImageIndex]);
+            var newFileName = Path.GetFileName(sourceFolder) + skipFileIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
+            var destinationPath = Path.Combine(destinationFolder, newFileName);
+            while (File.Exists(destinationPath))
             {
-                var destinationFolder = Path.Combine(sourceFolder, Path.GetFileName(sourceFolder) + "_" + folderIndex.ToString());
-
-                Directory.CreateDirectory(destinationFolder);
-
-                var fileName = Path.GetFileName(imageFile);
-                var newFileName = imageIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
-                var destinationPath = Path.Combine(destinationFolder, newFileName);
-
-                Console.WriteLine("Moving " + imageFile + " to " + destinationPath);
-
-                await Task.Run(() => File.Move(imageFile, destinationPath));
-
-                fileIndex++;
-                imageIndex++;
-
-                folderIndex++;
-
-                if (fileIndex % 18 == 0)
-                {
-                    folderIndex = 1;
-                }
+                skipFileIndex++;
+                newFileName = Path.GetFileName(sourceFolder) + skipFileIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
+                destinationPath = Path.Combine(destinationFolder, newFileName);
             }
+            File.Move(imageFiles[currentImageIndex], destinationPath);
+            currentImageIndex++;
+            skipFileIndex++;
+            ShowCurrentImage();
         }
     }
 }

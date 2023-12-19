bd60c83655f6d6d9499fee5f5ae954d47cfa5d87
koki uehara <koki.uehara@hutzper.com>
2023-12-19 12:34:14 +0900

    変更: 削除機能をOK機能に変更し、スキップ機能を改善
    - 削除ボタン(btnDelete)をOKボタン(btnOk)に変更し、その機能を画像を特定のフォルダ('ProcessedImages')に移動するように修正。
    - スキップボタン(btnSkip)の機能を改善し、スキップされた画像を別のフォルダ('SkippedImages')に移動するように変更。
    - 画像の移動ロジックを共通化し、コードの重複を減らすためにMoveImageメソッドを導入。
    - 画像のインデックス管理を改善し、ファイル名の生成ロジックを更新。
    
    これらの変更により、ユーザーは画像をより効率的に整理し、必要に応じて特定のフォルダに分類することができるようになります。

--- MainForm.cs
+++ MainForm.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 
 namespace ImageSortApp
 {
-    // メインフォーム
     public partial class MainForm : Form
     {
         public MainForm()
@@ -14,113 +13,73 @@ namespace ImageSortApp
             InitializeComponent();
         }
 
-        // ドラッグアンドドロップのエンターイベント
         private void MainForm_DragEnter(object sender, DragEventArgs e)
         {
-            // ドラッグアンドドロップのデータがファイルであるか確認
             if (e.Data != null && e.Data.GetDataPresent(DataFormats.FileDrop))
                 e.Effect = DragDropEffects.Copy;
         }
 
-        // ドラッグアンドドロップのドロップイベント
         private void MainForm_DragDrop(object sender, DragEventArgs e)
         {
-            // ドラッグアンドドロップされたファイルを取得
             if (e.Data != null)
             {
                 var files = (string[])e.Data.GetData(DataFormats.FileDrop);
-
-                // 画像ファイルを取得
                 var imageFiles = Directory.GetFiles(files[0], "*.*", SearchOption.TopDirectoryOnly)
                     .Where(s => s.EndsWith(".png") || s.EndsWith(".jpg") || s.EndsWith(".jpeg") || s.EndsWith(".bmp") || s.EndsWith(".gif")).ToArray();
 
-
-                // 画像ファイルが存在する場合
                 if (imageFiles.Length > 0)
                 {
-                    // 画像フォームを作成し、表示
                     var imageForm = new ImageForm(imageFiles, files[0]);
                     imageForm.Show();
                 }
                 else
                 {
-                    // 画像ファイルが存在しない場合、メッセージを表示
                     MessageBox.Show("画像ファイルがありません");
                 }
             }
             else
             {
-                MessageBox.Show("フォルダを確認にしてください。");
+                MessageBox.Show("フォルダを確認してください。");
             }
         }
     }
 
-    // 画像フォーム
     public class ImageForm : Form
     {
         private string[] imageFiles;
         private string sourceFolder;
         private int currentImageIndex = 0;
         private PictureBox pictureBox;
-        private Button btnDelete;
+        private Button btnOk;
         private Button btnSkip;
+        private int okFileIndex = 1;
         private int skipFileIndex = 1;
 
         public ImageForm(string[] imageFiles, string sourceFolder)
         {
-            // 初期化
             this.imageFiles = imageFiles;
             this.sourceFolder = sourceFolder;
 
-            // PictureBoxを作成
             this.pictureBox = new PictureBox { Dock = DockStyle.Fill, SizeMode = PictureBoxSizeMode.Zoom };
             this.Controls.Add(pictureBox);
 
-            // 削除ボタンを作成
-            this.btnDelete = new Button { Text = "Delete", Dock = DockStyle.Bottom, Height = 50 };
-            this.btnDelete.Click += btnDelete_Click;
-            this.Controls.Add(btnDelete);
+            this.btnOk = new Button { Text = "OK", Dock = DockStyle.Bottom, Height = 50 };
+            this.btnOk.Click += btnOk_Click;
+            this.Controls.Add(btnOk);
 
-            // スキップボタンを作成
             this.btnSkip = new Button { Text = "Skip", Dock = DockStyle.Bottom, Height = 50 };
             this.btnSkip.Click += btnSkip_Click;
             this.Controls.Add(btnSkip);
 
-            // 移動先のフォルダを作成
-            var destinationFolder = Path.Combine(sourceFolder, "SkippedImages");
-            if (Directory.Exists(destinationFolder))
-            {
-                // 移動先のフォルダ内のファイルを取得
-                var existingFiles = Directory.GetFiles(destinationFolder, "*.*", SearchOption.TopDirectoryOnly)
-                    .Where(s => s.StartsWith(Path.GetFileName(sourceFolder)));
-
-                // 移動先のフォルダ内にファイルが存在する場合
-                if (existingFiles.Any())
-                {
-                    // 最後のファイル名を取得
-                    var lastFileName = existingFiles.OrderBy(f => f).Last();
-
-                    // 最後のファイル名から連番を取得
-                    var lastFileNumber = int.Parse(Path.GetFileNameWithoutExtension(lastFileName).Substring(Path.GetFileName(sourceFolder).Length, 4));
-
-                    // 連番を更新
-                    skipFileIndex = lastFileNumber + 1;
-                }
-            }
-
             this.Size = new Size(1240, 1240);
 
-            // 現在の画像を表示
             ShowCurrentImage();
         }
 
-        // 現在の画像を表示
         private void ShowCurrentImage()
         {
-            // 画像ファイルが存在する場合
             if (currentImageIndex < imageFiles.Length)
             {
-                // 画像を読み込み、表示
                 using (var img = new Bitmap(imageFiles[currentImageIndex]))
                 {
                     pictureBox.Image = new Bitmap(img);
@@ -128,55 +87,40 @@ namespace ImageSortApp
             }
             else
             {
-                // 画像ファイルが存在しない場合、メッセージを表示し、フォームを閉じる
                 MessageBox.Show("画像ファイルがありません");
                 this.Close();
             }
         }
 
-        // 削除ボタンのクリックイベント
-        private void btnDelete_Click(object sender, EventArgs e)
+        private void btnOk_Click(object sender, EventArgs e)
         {
-            // 現在の画像ファイルを削除
-            File.Delete(imageFiles[currentImageIndex]);
-
-            // 次の画像へ
-            currentImageIndex++;
-
-            // 画像を表示
-            ShowCurrentImage();
+            MoveImage("ProcessedImages", ref okFileIndex);
         }
 
-        // スキップボタンのクリックイベント
         private void btnSkip_Click(object sender, EventArgs e)
         {
-            // 移動先のフォルダを作成
-            var destinationFolder = Path.Combine(sourceFolder, "SkippedImages");
+            MoveImage("SkippedImages", ref skipFileIndex);
+        }
+
+        private void MoveImage(string folderName, ref int fileIndex)
+        {
+            var destinationFolder = Path.Combine(sourceFolder, folderName);
             Directory.CreateDirectory(destinationFolder);
 
-            // 新しいファイル名を作成
             var fileName = Path.GetFileName(imageFiles[currentImageIndex]);
-            var newFileName = Path.GetFileName(sourceFolder) + skipFileIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
+            var newFileName = Path.GetFileName(sourceFolder) + fileIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
             var destinationPath = Path.Combine(destinationFolder, newFileName);
 
-            // 同じ名前のファイルが存在する場合は、連番を更新
             while (File.Exists(destinationPath))
             {
-                skipFileIndex++;
-                newFileName = Path.GetFileName(sourceFolder) + skipFileIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
+                fileIndex++;
+                newFileName = Path.GetFileName(sourceFolder) + fileIndex.ToString("D4") + fileName.Substring(fileName.IndexOf("_"));
                 destinationPath = Path.Combine(destinationFolder, newFileName);
             }
 
-            // ファイルを移動
             File.Move(imageFiles[currentImageIndex], destinationPath);
-
-            // 次の画像へ
             currentImageIndex++;
-
-            // 連番を更新
-            skipFileIndex++;
-
-            // 画像を表示
+            fileIndex++;
             ShowCurrentImage();
         }
     }
